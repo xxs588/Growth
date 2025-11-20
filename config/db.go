6848253c3a29ebd15b564a7be5ecd94a1db784d3
xxs588 <2127069700@qq.com>
2025-11-20 20:18:22 +0800

    feat:连接数据库

---
+++ config/db.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"mygo/model"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	var err error
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("连接数据库失败:", err)
+	}
+
+	// 自动迁移表结构
+	err = DB.AutoMigrate(&model.User{}, &model.VerificationCode{})
+	if err != nil {
+		log.Fatal("迁移数据库失败:", err)
+	}
+	fmt.Println("数据库连接成功")
+}
